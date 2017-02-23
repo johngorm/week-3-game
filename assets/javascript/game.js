43bfe04d0e1ba5cfbea85042506331009734a059
John Gorman <johngorm@usc.edu>
2017-02-22 20:21:56 -0600

    Make game object which saves player info

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,59 +1,55 @@
 
-function pickSolution() {
-	var index = Math.floor(Math.random()*(solutionBank.length) - 1);
-	return solutionBank[index];
-}
+var game = {
+	solutionBank : ["mario", "sonic", "link", "ness", "samus", "jigglypuff", "yoshi", "zelda", "pokeball", "marth", 
+		"ganon", "pikachu" , "bowser", "fox", "wario" ],
+	displayedSolution : [],
+	
+	pickSolution : function() {
+		var index = Math.floor(Math.random()*(this.solutionBank.length) - 1);
+		this.solution = this.solutionBank[index];
+	},
+	guessedLetters: [],
 
-var solutionBank = ["mario", "sonic", "link", "ness", "samus", "jigglypuff", "yoshi", "zelda", "pokeball", "marth", 
-		"ganon", "pikachu" , "bowser", "fox", "wario" ];
-var displayedSolution = []; /*array of blanks that is same size as solution,
-						will reveal letters with correct guesses*/ 
-var solution;
-var guessedLetters = []; /*array*/
-var incorrectGuesses = [];
-var wins = 0;
-var guessRemain = 10;
+	incorrectGuesses: [],
+	wins: 0,
+	guessRemain: 10
+};
 
-solution = pickSolution();
 
-var split_solution = solution.split("");
-for(var i = 0; i < solution.length; i++)
-{
-	displayedSolution[i] = "-";
-}
-
-updateScreen();
-/*var solution_space = document.getElementById("solution");
-
-solution_space.innerHTML = displayedSolution.join('');
-
-document.getElementById("remaining_guesses").innerHTML = guessRemain;*/
 
 function updateScreen(){
-	document.getElementById("incorrect_letters").innerHTML =  incorrectGuesses.toString() ;
-	document.getElementById("remaining_guesses").innerHTML =  guessRemain ;
-	document.getElementById("win_counter").innerHTML = "Wins: " + wins; 
-	document.getElementById("solution").innerHTML = displayedSolution.join('');
+	document.getElementById("incorrect_letters").innerHTML =  game.incorrectGuesses.toString() ;
+	document.getElementById("remaining_guesses").innerHTML =  game.guessRemain ;
+	document.getElementById("win_counter").innerHTML = "Wins: " + game.wins; 
+	document.getElementById("solution").innerHTML = game.displayedSolution.join('');
 }
 
 
 function resetGame () {
 	console.log("foo");
-	guessRemain = 10;
-	wins = 0;
-	solution = pickSolution();
-	guessedLetters = [];
-	incorrectGuesses = [];
-	displayedSolution = [];
+	game.guessRemain = 10;
+	game.wins = 0;
+	game.pickSolution();
+	game.guessedLetters = [];
+	game.incorrectGuesses = [];
+	game.displayedSolution = [];
 	for(var i = 0; i < solution.length; i++)
 	{
-		displayedSolution[i] = "-";
+		game.displayedSolution[i] = "-";
 	}
 	updateScreen();
 
-	split_solution = solution.split("");
 
 };
+
+game.pickSolution();
+
+for(var i = 0; i < game.solution.length; i++)
+{
+	game.displayedSolution[i] = "-";
+}
+
+updateScreen();
 
 
 
@@ -65,9 +61,9 @@ document.onkeyup = function(){
 	var isOldGuess = false;
 	/*test keystroke to see if it already was pressed*/
 
-	for(var ii = 0; ii < guessedLetters.length; ii++){
+	for(var ii = 0; ii < game.guessedLetters.length; ii++){
 
-		if(guessedLetters[ii] == userguess){
+		if(game.guessedLetters[ii] == userguess){
 			isOldGuess= true;
 			break;
 
@@ -77,54 +73,44 @@ document.onkeyup = function(){
 	if(!isOldGuess){
 
 
-		for(var jj = 0; jj < split_solution.length; jj++){
+		for(var jj = 0; jj < game.solution.length; jj++){
 		
-			if(split_solution[jj] == userguess)
+			if(game.solution[jj] == userguess)
 			{
-
 				isGuessInSol = true;
-				displayedSolution[jj] = userguess;
-				console.log(displayedSolution);
+				game.displayedSolution[jj] = userguess;
+				console.log(game.displayedSolution);
 			}
 		}
 
 		if(isGuessInSol){
-			displayedSolution[char_position] = userguess;
-			/*solution_space.innerHTML = displayedSolution.join('');
+			game.displayedSolution[char_position] = userguess;
+			
 			/*check to see if word is complete*/
-			updateScreen();
-			if(displayedSolution.toString() === split_solution.toString()){
+			
+			if(game.displayedSolution.join('') === game.solution.toString()){
 				/*win state*/
 				console.log("Win");
-				wins++;
-				document.getElementById("win_counter").innerHTML = "Wins: " + wins;
-				return;
-			}
-			else{
+				game.wins++;
+				updateScreen();
 				
-				console.log("split: " + split_solution + " " + split_solution.length);
-				console.log("Correct");
-
+				return;
 			}
 		}
 		else{
 			console.log("Wrong guess");
-			incorrectGuesses.push(userguess);
-			if(guessRemain == 1){
+			game.incorrectGuesses.push(userguess);
+			if(game.guessRemain == 1){
 				console.log("You lose");
-				wins--;
-				document.getElementById("win_counter").innerHTML = "Wins: " + wins;
+				game.wins--;
 				return;
 			}
-			guessRemain--;
+			game.guessRemain--;
 			console.log("Update guessed letters");
 			
-			
-
 		}
-		guessedLetters.push(userguess);
+		game.guessedLetters.push(userguess);
 		updateScreen();
-		
-		
+			
 	}
 };
